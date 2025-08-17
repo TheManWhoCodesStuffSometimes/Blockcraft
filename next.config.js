1874dcb217e07cd76c9813a7787670ba6049d341
TheManWhoCodesStuffSometimes <167024529+TheManWhoCodesStuffSometimes@users.noreply.github.com>
2025-08-17 15:49:19 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    appDir: true
-  }
+  reactStrictMode: true
+  // No experimental.appDir needed in Next 14+
 };
-
 module.exports = nextConfig;
